f7050fb9c2f49479406cb140639e46bd1df88602
Kseniia <bacteriofan@gmail.com>
2018-02-18 22:10:03 +0300

    Initial commit

---
+++ src/javase01/t01/main/Logic.java
@@ -0,0 +1,10 @@
+package javase01.t01.main;
+
+    public class Logic {
+        public String method(){
+            return "I am string in Logic.";
+        }
+
+    }
+
+
---
+++ src/task6/RecordInNotepad.java
@@ -0,0 +1,9 @@
+package task6;
+
+import java.util.ArrayList;
+
+public class RecordInNotepad {
+
+    ArrayList<String> records = new ArrayList();
+
+}
